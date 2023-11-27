6ae899e19208a4d6b8e97e7de9b7fe1acb68d209
Kanaiyajee Gupta <kishan8798gupta@gmail.com>
2023-11-27 10:43:54 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+const textbox = document.getElementById("search");
+const list = document.getElementById("list-container");
+
+function addtask() {
+    if (textbox.value === '') {
+        alert("You must fill this field");
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = textbox.value;
+        list.appendChild(li);
+        let span = document.createElement("span")
+        span.innerHTML = "\u00d7"
+        li.appendChild(span)
+
+    }
+    textbox.value = "";
+    save();
+}
+    list.addEventListener("click", function (e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        save();
+    }
+    else if (e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        save();
+    }
+}, false);
+
+function save() {
+    localStorage.setItem("data", list.innerHTML);
+}
+function showList() {
+    list.innerHTML = localStorage.getItem("data");
+}
+showList();
